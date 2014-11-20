c4feefd61774d8ef7993b8c40f79c51baa9e08ca
Sebastian Muszyński <muszynski.sebastian@gmail.com>
2014-11-20 01:38:27 +0100

    Added info about customers number to Main class.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("just testing git");
 		Shop shop = new Shop();
+		System.out.println("Shop info:");
 		System.out.println("Total number of purchased apps: " + shop.purchasedAppsNumber());
+		System.out.println("Total number of customers: " + shop.customersNumber());
 	}
 
 }
