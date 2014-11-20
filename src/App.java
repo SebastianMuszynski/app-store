efae2d99b1bb22bcae40490b7330e04cbae803d6
Sebastian Muszyński <muszynski.sebastian@gmail.com>
2014-11-20 01:18:35 +0100

    Added first variables to the App class based on documentation.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,16 @@
 
 public class App {
-
+	private String developerName;
+	private String name;
+	private String description;
+	private int purchasesNumber; // the popularity of the app
+	private String category; // News, Games, Social Networking, Travel, etc.
+	private String type; // free trial OR for purchasing
+	
+	/*
+	 * TODO: work out the cost of the app
+	 */
+	public double cost() {
+		return 0;
+	}
 }
