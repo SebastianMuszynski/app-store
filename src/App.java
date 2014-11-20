a99d213e0840e3ba6c29ca1ed441ed79b1f58506
Sebastian Muszyński <muszynski.sebastian@gmail.com>
2014-11-20 01:44:22 +0100

    Added constructors for App and Customer classes.

--- src/App.java
+++ src/App.java
@@ -1,11 +1,19 @@
 
 public class App {
-	private String developerName;
 	private String name;
 	private String description;
-	private int purchasesNumber; // the popularity of the app
+	private String developerName;
 	private String category; // News, Games, Social Networking, Travel, etc.
 	private String type; // free trial OR for purchasing
+	private int purchasesNumber; // the popularity of the app
+	
+	public App(String name, String description, String developerName, String category, String type) {
+		this.name = name;
+		this.description = description;
+		this.developerName = developerName;
+		this.category = category;
+		this.type = type;
+	}
 	
 	/**
 	 * TODO: work out the cost of the app
--- src/Customer.java
+++ src/Customer.java
@@ -3,5 +3,11 @@ public class Customer {
 	private String name;
 	private String address;
 	private String profession;
+	
+	public Customer(String name, String address, String profession) {
+		this.name = name;
+		this.address = address;
+		this.profession = profession;
+	}
 
 }
