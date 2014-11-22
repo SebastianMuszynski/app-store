59a31939850241e4656503e2fefa0734d35c26cf
Mali <malii.coxon@gmail.com>
2014-11-22 16:29:43 +0000

    Added app enumeration demonstration

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,11 @@ public class Main {
 		System.out.println("Student info:");
 		System.out.println("Number of free apps: " + student.numberOfFreeApps());
 		System.out.println("Number of paid apps: " + student.numberOfPaidApps());
+		
+		
+		// app enum testing
+		FreeApp app = new FreeApp("Flappy Bird", "Flapping bird game.", "Dev Name", Category.NEWS_APP);
+		System.out.println("App type is " + app.getCategory());
 	}
 
 }
